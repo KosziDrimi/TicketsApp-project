c637a6f4db1c80df952217618b5e32174dec752f
Katarzyna Klimiuk <klimiuk.kasia@gmail.com>
2022-01-30 15:51:17 +0100

    UserView added

--- api/urls.py
+++ api/urls.py
@@ -8,9 +8,14 @@ router = routers.DefaultRouter()
 router.register('types', views.TicketTypeView)
 router.register('events', views.EventView)
 router.register('prices', views.PriceView)
-router.register('orders', views.OrderView)
+router.register('orders', views.OrderView, basename='order')
 router.register('tickets', views.TicketView)
+router.register('users', views.UserView)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+]
+
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
 ]
--- api/views.py
+++ api/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
+from django.contrib.auth.models import User
 
 from .models import TicketType, Event, Price, Order, Ticket
-from .serializers import TicketTypeSerializer, EventSerializer, PriceSerializer, OrderSerializer, TicketSerializer
+from .serializers import TicketTypeSerializer, EventSerializer, PriceSerializer, OrderSerializer, TicketSerializer,\
+                         UserSerializer
 
 
 class TicketTypeView(viewsets.ModelViewSet):
@@ -25,9 +26,17 @@ class OrderView(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
 class TicketView(viewsets.ModelViewSet):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ('event', 'price__ticket_type')
+
+
+class UserView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
