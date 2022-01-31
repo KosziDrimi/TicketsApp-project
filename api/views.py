97eee91f5c8a57f98623a1568aac3b632308abb6
Katarzyna Klimiuk <klimiuk.kasia@gmail.com>
2022-01-31 09:02:17 +0100

    readonly permission added

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets
+from rest_framework.permissions import BasePermission, IsAdminUser, SAFE_METHODS
 from django_filters.rest_framework import DjangoFilterBackend
 from django.contrib.auth.models import User
 
@@ -7,21 +8,27 @@ from .serializers import TicketTypeSerializer, EventSerializer, PriceSerializer,
                          UserSerializer
 
 
+class ReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS
+
+
 class TicketTypeView(viewsets.ModelViewSet):
     queryset = TicketType.objects.all()
     serializer_class = TicketTypeSerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [IsAdminUser]
 
 
 class EventView(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+    permission_classes = [IsAdminUser|ReadOnly]
 
 
 class PriceView(viewsets.ModelViewSet):
     queryset = Price.objects.all()
     serializer_class = PriceSerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [IsAdminUser]
 
 
 class OrderView(viewsets.ModelViewSet):
@@ -42,9 +49,10 @@ class TicketView(viewsets.ModelViewSet):
     serializer_class = TicketSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ('event', 'price__ticket_type')
+    permission_classes = [IsAdminUser | ReadOnly]
 
 
 class UserView(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [IsAdminUser]
