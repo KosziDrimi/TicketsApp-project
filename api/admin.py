1ba7005667284ad4193557d46daebccce1954770
Katarzyna Klimiuk <klimiuk.kasia@gmail.com>
2022-04-11 11:20:55 +0200

    admin panel improved

--- api/admin.py
+++ api/admin.py
@@ -1,10 +1,29 @@
 from django.contrib import admin
 
-from .models import TicketType, Event, Price, Order, Ticket
+from .models import Event, Order, Price, Ticket, TicketType
 
 
-admin.site.register(TicketType)
-admin.site.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['name', 'datetime']
+
+
+class TicketInline(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['order_number', 'is_paid', 'is_valid']
+    inlines = [TicketInline]
+
+
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ['serial_number', '__str__', 'order']
+    ordering = ['order']
+
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(Price)
-admin.site.register(Order)
-admin.site.register(Ticket)
+admin.site.register(Ticket, TicketAdmin)
+admin.site.register(TicketType)
